0f83c60b628188fd559a64bb77d013ac9524337b
Marco Cantu Gea <marco.cantu@gme.mx>
2021-09-25 10:57:14 -0500

    Primer ejercicio php

---
+++ Ejercicios/Ejercicio1.php
@@ -0,0 +1,12 @@
+<?php
+
+    //crear un archivo csv de los datos /data/estatos_diccionario.json
+    // en el archivo se encuentra el estado y combinaciones que hacen referencia al estado
+    // se requiere realizar un sumarizado por estado para ver cuantas combinaciones existen
+    // por cada estado, se require que se crea un csv con el sumarizado de la siguiente manera
+    /*
+        edo_id,      edo_nombre,      total_combinaciones
+        4,           Campeche,        9
+    */
+
+?>
